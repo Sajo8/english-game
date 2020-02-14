cc810c1797808e9f6e96ac996e89b961336288f1
Andrew Splin <andrewsplin@protonmail.com>
2020-02-14 15:48:00 -0700

    Finish first word

--- GUI/screens/TitleScreen.gd
+++ GUI/screens/TitleScreen.gd
@@ -1,17 +1,21 @@
 extends Control
 
+export var scene : PackedScene = load("res://words/Word1.tscn")
+
 func _ready() -> void:
 	# Maximise window
 	OS.set_window_maximized(true)
 	# Focus on the Play button on start
 	$VBoxContainer/Play.grab_focus()
 
+	Globals.add_powerup("double_jump")
+
 func _on_Play_pressed() -> void:
 	# Fade in, and go to Level1
 	$AnimationPlayer.play("fade_in")
 	yield($AnimationPlayer, "animation_finished")
 
-	get_tree().change_scene("res://levels/level1/Level1.tscn")
+	get_tree().change_scene_to(scene)
 
 func _on_Kahoot_pressed() -> void:
 	OS.shell_open("https://kahoot.com")
--- sceneElements/spikes/Spikes.gd
+++ sceneElements/spikes/Spikes.gd
@@ -1,19 +1,5 @@
 extends Area2D
 
-# Export two options for the sprite of the spikes
-export(String, "Low", "High") var spikes_type = "High"
-
-func _ready() -> void:
-	# Load the set sprite version
-	var spikes_string = "res://assets/included/Other/spikes" + spikes_type + ".png"
-	$Sprite.texture = load(spikes_string)
-
-	# Change collisionshape based on which sprite version is chosen
-	if spikes_string == "Low":
-		$CollisionShape2D.position = Vector2(0, -3)
-	else:
-		$CollisionShape2D.position = Vector2(0, -5)
-
 func _on_Spikes_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
 		# Kill player upon impact
