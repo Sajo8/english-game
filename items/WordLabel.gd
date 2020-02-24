d1511cd9e1e2e3e0fd13e8d7e34c422530508da5
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-02-23 22:26:52 -0700

    change word 4 to red
    ideally:
    1 - 3: blue
    4 - 6: red
    6 - 8: green
    8 - 10: yellow

--- items/WordLabel.gd
+++ items/WordLabel.gd
@@ -1,5 +1,13 @@
 extends Label
 
+export var dark := true
+
+func _ready() -> void:
+	if dark:
+		set_theme(load("res://GUI/themes/DarkLabelMid.tres"))
+	else:
+		set_theme(load("res://GUI/themes/WhiteLabelMid.tres"))
+
 func fade() -> void:
 	$AnimationPlayer.play("fade")
 	yield($AnimationPlayer, "animation_finished")
